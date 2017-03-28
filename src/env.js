6698b459a3ccf5df9a06a01e5c7f0d3c6ac75ce3
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-03-29 02:43:45 +0300

    Add the environment variables processing

---
+++ src/env.js
@@ -0,0 +1,48 @@
+import dotenv from 'dotenv'
+import util from 'util'
+import logger from './logger'
+
+function updateEnv() {
+  const result = dotenv.config()
+  if (typeof result.error !== 'undefined') {
+    logger.warn(`unable to load the .env file: ${util.inspect(result.error)}`)
+  }
+}
+
+function testEnv(result_name, variables, incorrects_filter) {
+  const incorrect_variables = variables.filter(incorrects_filter)
+  if (incorrect_variables.length !== 0) {
+    logger.error(
+      `following environment variables are ${result_name}: `
+        + incorrect_variables.join(', '),
+    )
+
+    process.exit(1)
+  }
+}
+
+const REQUIRED_VARIABLES = [
+  'VK_BOT_TOKEN',
+]
+function checkRequiredEnv() {
+  testEnv('required', REQUIRED_VARIABLES, variable => {
+    return typeof process.env[variable] === 'undefined'
+  })
+}
+
+const VARIABLES_PATTERNS = {
+  VK_BOT_TOKEN: /.+/,
+}
+function validateEnv() {
+  testEnv('incorrect', Object.keys(process.env), variable => {
+    return typeof VARIABLES_PATTERNS[variable] !== 'undefined'
+      && !VARIABLES_PATTERNS[variable].test(process.env[variable])
+  })
+}
+
+export default function processEnv() {
+  updateEnv()
+
+  checkRequiredEnv()
+  validateEnv()
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import 'babel-polyfill'
+import processEnv from './env'
 import logger from './logger'
 
+processEnv()
 logger.info('Hello, world!')
