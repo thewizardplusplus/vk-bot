5554c7dd0fa1f52f0776561d4a40a4120ebc8f7a
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-03-30 13:47:30 +0300

    Issue #2: extract attachments from a command response

--- src/attachments.js
+++ src/attachments.js
@@ -1,12 +1,32 @@
 import util from 'util'
 import logger from './logger'
 
+const ATTACHMENT_PATTERN = /\${file:\/\/([^{}]+)}/g
+function extractAttachments(response) {
+  const attachments = []
+  const cleaned_response = ATTACHMENT_PATTERN[Symbol.replace](
+    response,
+    (attachment, path) => {
+      attachments.push(path)
+      return ''
+    },
+  )
+
+  return {
+    cleaned_response,
+    attachments,
+  }
+}
+
 export default function makeAttachmentsHandler() {
   return (vk_bot, response) => {
     logger.info(`response has been received: ${util.inspect(response)}`)
 
+    const {cleaned_response, attachments} = extractAttachments(response)
+    logger.debug(`extracted attachments: ${util.inspect(attachments)}`)
+
     return {
-      message: response,
+      message: cleaned_response,
       attachments: [
         'photo-143852874_456239017',
         'photo-143852874_456239018',
