97d92c334d020e08302087bfb65120bba0dec738
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-04-02 10:27:56 +0300

    Issue #16: simplify an environment variables processing

--- src/env.js
+++ src/env.js
@@ -9,44 +9,27 @@ function updateEnv() {
   }
 }
 
-function testEnv(result_name, variables, incorrects_filter) {
-  const incorrect_variables = variables.filter(incorrects_filter)
-  if (incorrect_variables.length !== 0) {
+const REQUIRED_VARIABLES = [
+  'VK_BOT_TOKEN',
+  'VK_BOT_GROUP',
+  'VK_BOT_COMMAND',
+]
+function validateEnv() {
+  const missed_variables = REQUIRED_VARIABLES.filter(variable => {
+    return typeof process.env[variable] === 'undefined'
+      || process.env[variable].length === 0
+  })
+  if (missed_variables.length !== 0) {
     logger.error(
-      `following environment variables are ${result_name}: `
-        + incorrect_variables.join(', '),
+      `following environment variables are required: `
+        + missed_variables.join(', '),
     )
 
     process.exit(1)
   }
 }
 
-const REQUIRED_VARIABLES = [
-  'VK_BOT_TOKEN',
-  'VK_BOT_GROUP',
-  'VK_BOT_COMMAND',
-]
-function checkRequiredEnv() {
-  testEnv('required', REQUIRED_VARIABLES, variable => {
-    return typeof process.env[variable] === 'undefined'
-  })
-}
-
-const VARIABLES_PATTERNS = {
-  VK_BOT_TOKEN: /.+/,
-  VK_BOT_GROUP: /.+/,
-  VK_BOT_COMMAND: /.+/,
-}
-function validateEnv() {
-  testEnv('incorrect', Object.keys(process.env), variable => {
-    return typeof VARIABLES_PATTERNS[variable] !== 'undefined'
-      && !VARIABLES_PATTERNS[variable].test(process.env[variable])
-  })
-}
-
 export default function processEnv() {
   updateEnv()
-
-  checkRequiredEnv()
   validateEnv()
 }
