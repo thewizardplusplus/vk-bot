a51f85c7a5e6b1eec739c73c329b5daad35a65f9
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-04-10 19:29:38 +0300

    Issue #22: process errors representations

--- src/logger.js
+++ src/logger.js
@@ -12,9 +12,18 @@ export const logger = new winston.Logger({
   exitOnError: false,
 })
 
+const LINE_BREAK = /[\n\r]+/g
+const WHITESPACES = /\s+/g
 export function inspect(object) {
-  return util.inspect(object, {
+  let representation = util.inspect(object, {
     breakLength: Infinity,
     colors: process.env.VK_BOT_COLORFUL_LOG === 'TRUE',
   })
+  // if the representation contains line breaks,
+  // likely it's an error representation
+  if (representation.search(LINE_BREAK) !== -1) {
+    representation = representation.replace(WHITESPACES, ' ')
+  }
+
+  return representation
 }
