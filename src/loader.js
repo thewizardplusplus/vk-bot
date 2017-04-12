aad16fdb63261086dd99471c0da0e4a87183752c
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-04-12 20:12:42 +0300

    Issue #25: add a support of a log prefix to the makeCachedAttachmentLoader() function

--- src/loader.js
+++ src/loader.js
@@ -2,26 +2,33 @@ import {logger, inspect} from './logger'
 import {makeAttachmentId} from './attachments'
 
 export default function makeCachedAttachmentLoader(cache) {
-  return (vk_bot, path) => {
+  return (vk_bot, path, log_prefix = '') => {
     const attachment = cache.get(
       path,
       process.env.VK_BOT_INEXACT_CACHE === 'TRUE',
     )
     if (attachment !== null) {
-      logger.info(`attachment ${inspect(path)} has been found in the cache`)
+      logger.info(
+        `${log_prefix}attachment ${inspect(path)} has been found in the cache`,
+      )
+
       return attachment.attachment
     }
 
     return vk_bot
       .uploadPhoto(path)
       .then(({owner_id, id}) => {
-        logger.info(`attachment ${inspect(path)} has been loaded`)
+        logger.info(`${log_prefix}attachment ${inspect(path)} has been loaded`)
 
         const attachment_id = makeAttachmentId(owner_id, id)
-        return cache.addAndSave(attachment_id, path)
+        return cache.addAndSave(attachment_id, path, log_prefix)
       })
       .then(({attachment}) => {
-        logger.info(`attachment ${inspect(path)} has been added to the cache`)
+        logger.info(
+          `${log_prefix}attachment ${inspect(path)} `
+            + `has been added to the cache`,
+        )
+
         return attachment
       })
   }
